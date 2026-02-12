b7c506c263ad18f57090b0fa0c6827acaa34d9f8
moad6039 <muaadh.al.ammari@gmail.com>
2026-02-12 15:14:03 +0100

    route hashtage supprime dans la route tweet

--- routes/tweets.js
+++ routes/tweets.js
@@ -11,16 +11,5 @@ router.get("/", function (req, res, next) {
     .then((tweet) => res.json({ tweet: tweet }));
 });
 
-/* GET tweets by #hashtag */
-router.get("/:hashtag", function (req, res) {
-  Hashtag.find({ name: req.params.hashtag }).then((data) => {
-    console.log("data", data);
-    if (data.length > 0) {
-      res.json({ result: true, tweets: data });
-    } else {
-      res.json({ result: false, error: "hashtag not found" });
-    }
-  });
-});
 
 module.exports = router;
