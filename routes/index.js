f18ee276d42e6aaa809ae9a0319649d7c1bce620
loicvoireau <loicvoireau3@gmail.com>
2026-02-12 11:57:26 +0100

    Création models tweet et route tweet & hastags

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Tweet = require("../models/tweets")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Tweet.find()
+    .then((tweet) => res.json({tweet: tweet}))
 });
+
+/* GET #hashtag */
+router.get('/:hashtag', function(req, res) {
+  Tweet.findAll({hashtag: req.params.hashtag})
+  .then((tweets) => res.json({result: true, tweets: tweets}))
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const User = require("../models/users");
 const fetch = require('node-fetch');
+const { checkBody } = require('../modules/checkBody')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
