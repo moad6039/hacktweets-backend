8fc65c8aa38c68986df875084324cffc17d9f7e6
moad6039 <muaadh.al.ammari@gmail.com>
2026-02-12 15:09:02 +0100

    route Hashtage added

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,19 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Tweet = require("../models/tweets")
+var Tweet = require("../models/tweets");
+const Hashtag = require("../models/hashtags");
 
 /* GET all tweets */
-router.get('/', function(req, res, next) {
-    Tweet.find()
-    .populate('user')
-    .populate('hashtag')
-    .then((tweet) => res.json({tweet: tweet}))
+router.get("/", function (req, res, next) {
+  Tweet.find()
+    .populate("user")
+    .populate("hashtag")
+    .then((tweet) => res.json({ tweet: tweet }));
 });
 
 /* GET tweets by #hashtag */
-router.get('/:hashtag', function(req, res) {
-  Tweet.findAll({hashtag: req.params.hashtag})
-  .then((tweets) => res.json({result: true, tweets: tweets}))
-})
+router.get("/:hashtag", function (req, res) {
+  Hashtag.find({ name: req.params.hashtag }).then((tweets) =>
+    res.json({ result: true, tweets: tweets }),
+  );
+});
 
 module.exports = router;
