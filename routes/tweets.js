b991118b9ed45a97c104ffeed0e248cd0b144b43
loicvoireau <loicvoireau3@gmail.com>
2026-02-13 10:37:01 +0100

    création de la route post pour ajouter des tweets à la base de donnée

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Tweet = require("../models/tweets");
-const Hashtag = require("../models/hashtags");
+const { checkBody } = require("../modules/checkBody");
 
 /* GET all tweets */
 router.get("/", function (req, res, next) {
@@ -11,16 +11,24 @@ router.get("/", function (req, res, next) {
     .then((tweet) => res.json({ tweet: tweet }));
 });
 
-/* GET tweets by #hashtag */
-router.get("/:hashtag", function (req, res) {
-  Hashtag.find({ name: req.params.hashtag }).then((data) => {
-    console.log("data", data);
-    if (data.length > 0) {
-      res.json({ result: true, tweets: data });
-    } else {
-      res.json({ result: false, error: "hashtag not found" });
-    }
-  });
-});
+/* POST create tweet */
+router.post("/", function (req, res) {
+  if (!req.body.message) {
+    res.json({ result: false, error: 'empty fields' });
+    return;
+  }
+
+  const newTweet = new Tweet({
+        message: req.body.message,
+        nbLike: req.body.nbLike,
+        time: req.body.time,
+        user: req.body.user,
+        hashtag: req.body.hashtag
+      });
+  newTweet.save().then(data => {
+        res.json({ result: true });
+      });
+
+})
 
 module.exports = router;
